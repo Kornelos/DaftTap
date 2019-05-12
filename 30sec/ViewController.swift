ac002b4c1f1f90b5d556eea5082cce2907b38f1a
mikhosc <mikolaj.hoscilo@gmail.com>
2019-05-12 10:44:08 +0200

    Initial Commit

---
+++ 30sec/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  30sec
+//
+//  Created by Mikołaj Hościło on 12/05/2019.
+//  Copyright © 2019 Kornel Skórka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
